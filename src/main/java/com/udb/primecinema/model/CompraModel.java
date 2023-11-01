15284f437dbbc254e84547325fd749404d037e5b
Edgar Fernando Lopez Zavaleta <90074636+FerNIX1@users.noreply.github.com>
2023-11-01 15:38:45 -0600

    Se agrego dependencias en el pom,se creo carpeta META-INF,se edito WEB.xml y se crearon clases en la carpeta model

---
+++ src/main/java/com/udb/primecinema/model/CompraModel.java
@@ -0,0 +1,4 @@
+package com.udb.primecinema.model;
+
+public class CompraModel extends Conexion{
+}
---
+++ src/main/java/com/udb/primecinema/model/UsuariosModel.java
@@ -0,0 +1,11 @@
+package com.udb.primecinema.model;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class UsuariosModel extends Conexion {
+
+}
