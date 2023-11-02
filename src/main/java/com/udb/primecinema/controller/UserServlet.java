25947ba89bc881930406879ffd4b764eeceba52d
Edgar Fernando Lopez Zavaleta <90074636+FerNIX1@users.noreply.github.com>
2023-11-02 17:55:37 -0600

    Configuraciones en el web.xml y creacion del switch en UsuarioServelet

--- src/main/java/com/udb/primecinema/controller/UserServlet.java
+++ src/main/java/com/udb/primecinema/controller/UserServlet.java
@@ -5,9 +5,21 @@ import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "UserServlet", value = "/UserServlet")
+@WebServlet(name = "UserServlet", urlPatterns = {"/usuario.do"})
 public class UserServlet extends HttpServlet {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            String operacion = request.getParameter("op");
+            switch (operacion) {
+
+            }
+        }
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
